3fd2c289e60f9c9aed2ac35a4323d0ce23a980bb
gabjoao <joao.25.ribeiro01@gmail.com>
2023-06-26 17:51:30 -0300

    Atualização mobile

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+ 
+
 // Constantes das imagens
-const logo = document.querySelector('.imagem');
 const wallpaper = document.querySelector('.wallpaper');
 const capa1 = document.querySelector('.capa1');
 const capa2 = document.querySelector('.capa2');
@@ -25,35 +26,61 @@ const temas = {
 var x = Boolean()
 
 // A troca entre os temas
+
 if (input) {
     input.addEventListener('click', event => {
         event.preventDefault()
         console.log("funcionando")
         x = !x
-
         //
+
+       
         const temaAtual = document.body.dataset.theme
         document.body.setAttribute("data-theme", temas[temaAtual] || "claro")
 
+
+        updateWallpaper();
+
+
         if(x){
-            logo.setAttribute("src", 'media/logo-escuro.png')
             input.setAttribute("src", "media/symbol-escuro.png")
-            wallpaper.setAttribute("src", "media/wallpaper-escuro.jpg")
             capa1.setAttribute("src", "media/lis1-capa-escuro.png")
             capa2.setAttribute("src", "media/lis2-capa-escuro.png")
             capa3.setAttribute("src", "media/lis3-capa-escuro.png")
             
 
         } else {
-            logo.setAttribute("src", "media/logo-claro.png")
             input.setAttribute("src", "media/symbol-claro.png")
-            wallpaper.setAttribute("src", "media/wallpaper.jpg")
             capa1.setAttribute("src", "media/lis1-capa.jpg")
             capa2.setAttribute("src", "media/lis2-capa.jpg")
             capa3.setAttribute("src", "media/lis3-capa.jpeg")
         }
 
     })
+        
+}
 
+//Atualizando o wallpaper para celular
+updateWallpaper();
+function updateWallpaper() {
+    if(window.innerWidth <= 800 && x) {
+       wallpaper.setAttribute("src", "media/wallpaper-celular-escuro.jpg")
 
     }
+    else if(window.innerWidth <=800 && !x){
+        wallpaper.setAttribute("src", "media/wallpaper-celular.jpg")
+
+    }
+
+    else if(x){
+        wallpaper.setAttribute("src", "media/wallpaper-escuro.jpg")
+    }
+
+     else {
+        wallpaper.setAttribute("src", "media/wallpaper.jpg")
+        
+    }
+}
+
+
+window.addEventListener("resize", updateWallpaper); 
